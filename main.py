6835a53327b8237986cf99e234688c33a8eb15f4
kmj <mjoo1106@naver.com>
2022-04-17 20:32:48 +0900

    update question information from solve.ac

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ writer.writerow(list_title)
 
 user_agent = 'Your user agent information!!'
 headers={'User-Agent':user_agent}
-url = "https://www.acmicpc.net/status?top=" + str(start)
+url = "https://www.acmicpc.net/status?top=420000020"
 html = requests.get(url, headers=headers).text
 soup = BeautifulSoup(html, 'html.parser')
 nid = "solution-42000000"
@@ -20,3 +20,12 @@ tr = soup.find(id=nid)
 usr_url = "https://solved.ac/search/users?query=" + tds[1].text.strip()
 usr_html = requests.get(usr_url, headers=headers).text
 usr_soup = BeautifulSoup(usr_html, 'html.parser')
+# find queestion information from solved.ac
+que_url = "https://solved.ac/search?query=" + tds[2].text
+que_html = requests.get(que_url, headers=headers).text
+que_soup = BeautifulSoup(que_html, 'html.parser')
+que_title = que_soup.find(class_='__Latex__').text
+que_tier = que_soup.find(class_='TierBadge__TierBadgeStyle-sc-hiokan-0 puOTB')['alt']
+for algo in que_soup.find_all(class_='TagInline__TagDisplayNameContainer-sc-1tzcn2w-1 dKozlW'):
+    que_al_list.append(algo.text[1:])
+print(que_url, que_title, que_tier, que_al_list)
