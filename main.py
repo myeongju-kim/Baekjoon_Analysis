efb17f43d1f73acb1f336699faf7b1febe23fbcc
kmj <mjoo1106@naver.com>
2022-04-18 10:20:24 +0900

    change csv file name

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import csv
 from bs4 import BeautifulSoup
 import time
 
-filename='backjoon_trend.csv'
+filename='baekjoon_trend.csv'
 f=open(filename, 'w',encoding='utf-8-sig', newline='')
 list_title=['난이도','분류','문제번호','제목','사용자', '정답', '메모리', '시간', '언어','코드길이','제출날짜']
 writer=csv.writer(f)
